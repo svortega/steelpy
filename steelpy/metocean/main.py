5987903f8214835fd5ea2f3060a0f119df32b64e
Salvador Vargas-Ortega <svortega@gmail.com>
2022-01-10 13:05:18 +0000

    adding missing file in metoecan module

---
+++ steelpy/metocean/main.py
@@ -0,0 +1,112 @@
+# 
+# Copyright (c) 2019-2021 steelpy
+#
+# 
+# Python stdlib imports
+from typing import Union, Dict
+
+# package imports
+from steelpy.metocean.irregular.main import WaveIrregular
+from steelpy.metocean.regular.main import RegularWaves
+#from steelpy.metocean.irregular.spectrum import Sprectrum
+#from steelpy.process.units.units import Units
+#from steelpy.metocean.interface.wave import RegularWaves, IregularWaves
+#from steelpy.metocean.interface.wind import Winds
+#from steelpy.metocean.interface.current import Currents
+#from steelpy.f2uModel.load.combination import MetoceanCombination
+#
+#
+class Metocean:
+    """
+    FE Metocean Class
+    
+    Metocean
+        |_ name
+        |_ number
+        |_ data
+        |_ type
+        |_ wave
+        |_ current
+        |_ wind
+        |_ cdcm
+        |_ non_hydro
+        |_ elevation
+        |_ hydro_diameter
+        |_ buoyancy
+        |_ flooded
+        |_ seastate
+    
+    **Parameters**:  
+      :number:  integer internal number 
+      :name:  string node external name
+      
+      : seastate : metocean combination
+    """
+    #
+    __slots__ = ['regular_waves', 'iregular_waves',
+                 '_current', '_wind', '_units',
+                 '_combination', '_spectrum']
+    #
+    def __init__(self):
+        """
+        """
+        self.regular_waves = RegularWaves()
+        self.iregular_waves = WaveIrregular()
+        #self._wind = Winds()
+        #self._current = Currents()
+        #self._units = Units()
+        #self._combination = MetoceanCombination()
+        #self._spectrum = Sprectrum()
+    #
+    #@property
+    #def spectrum(self):
+    #    """
+    #    """
+    #    return self._spectrum
+    #
+    @property
+    def irregular_wave(self):
+        """
+        """
+        return self.iregular_waves    
+    #
+    @property
+    def regular_wave(self):
+        """
+        """
+        return self._regular_waves
+    #
+    #@property
+    #def linear_wave(self):
+    #    """
+    #    """
+    #    return self.regular_waves
+    #
+    #@property
+    #def deterministic_wave(self):
+    #    """
+    #    """
+    #    return self.regular_waves     
+    ##
+    #@property
+    #def wind(self):
+    #    """
+    #    """
+    #    return self._wind  
+    ##
+    #@property
+    #def current(self):
+    #    """
+    #    """
+    #    return self._current
+    ##
+    #@property
+    #def combination(self):
+    #    """
+    #    """
+    #    return self._combination    
+#
+#
+# HYDRODYNAMICS Section
+#
+
---
+++ steelpy/metocean/regular/main.py
@@ -0,0 +1,58 @@
+#
+# Copyright (c) 2009-2022 steelpy
+#
+# Python stdlib imports
+from typing import NamedTuple, Tuple, Union, List, Dict
+
+# package imports
+#from steelpy.metocean.regular.current.main_current import MeanCurrent
+from steelpy.metocean.regular.stokes.Stokes import StokesModule
+from steelpy.metocean.regular.fourier.Fourier import FourierModule
+from steelpy.metocean.regular.cnoidal.Cnoidal import CnoidalModule
+
+class RegularWaves:
+    __slots__ = ['_wave']
+
+    def __init__(self)-> None:
+        """
+        """
+        self._wave = StokesModule()
+    #
+    @property
+    def Stokes(self):
+        """ """
+        return StokesModule
+    #
+    @property
+    def Cnoidal(self):
+        """ """
+        return CnoidalModule    
+    #
+    @property
+    def Fourier(self):
+        """ """
+        return FourierModule
+    #
+    #def current(self):
+    #    """ """
+    #    return self._current
+    #
+    def __setitem__(self, case_name: int,
+                    case_data: Union[List[float], Dict[str, float]]) -> None:
+        """
+        case_name : Wave name
+         H : Wave height [unit length]
+         T : Wave Period [second]
+         d : Water depth LTH + Tide and Surge [unit length]
+         Lw : Wave Length [unit length]
+         Phase : Wave phase [degree]
+         Order : ??
+        """
+        self._wave[case_name] = case_data
+    #
+    #
+    def __getitem__(self, case_name: Union[int, str]) -> Tuple:
+        """
+        case_name : Wave name
+        """
+        return self._wave[case_name]
