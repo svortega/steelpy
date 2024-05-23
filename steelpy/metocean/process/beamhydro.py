ufo module : beam line load bug fixed --> L2 picking wrong value from database

steelpy/metocean/process/beamhydro.py
@@ -241,7 +241,39 @@ class BeamMorisonWave:
                        elev, dz)
         #return udl
     #
-
+    #
+    def Fwave2(self, Vc, MG, Cd, Cm,
+               kinematics, elev):
+        """
+        Wave force on a slender cilindrical element
+        
+        Vc : Current velocity
+        MG : Marine Growth
+        Cd : Drag Coefficient
+        Cm : Inertia Coefficient
+        WKF : Wave Kinematic Factor
+        kinematics : Kinematic class
+        nelev : number of elevations
+        """
+        #Elev = self.elevations(nelev=nelev)
+        Dh, At = self.Dh(mg=MG)
+        #dz = self.dz(nelev=nelev)
+        dz = np.diff(elev)
+        #
+        i = 0
+        for key, item in kinematics.items():
+            print(f'{key}')
+            #shape = item['ax'].shape
+            #Vc = permute1(Vc, order=shape[0])
+            kinacc = self.An(item)
+            kinvel = self.Un(item, Vc[i])
+            bforce = self.dF(Dh, At, Cd, Cm,
+                             kinvel, kinacc,
+                             elev, dz)
+            bforce
+            i += 1
+        #
+        1 / 0
 #
 #
 class BeamUnitForce(NamedTuple):

steelpy/metocean/wave/regular/process/kinematic.py
@@ -5,10 +5,12 @@
 from __future__ import annotations
 from array import array
 from dataclasses import dataclass
-from typing import NamedTuple
+#from typing import NamedTuple
 #import math
 
 # package imports
+from steelpy.utils.math.operations import linstep
+#
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.matlib import repmat
@@ -308,23 +310,6 @@ class KinematicResults:
     #
     # -------------------------------------------------------
     #
-    def kindf(self, items, xx, zdepth):
-        """ """
-        dfkin = np.zeros((xx.size, zdepth.size))
-        for i, step in enumerate(xx):
-            #new = []
-            item = items.get_group(name=(step, ))
-            for j, point in enumerate(zdepth):
-                dfkin[i, j] = np.interp(point,
-                                        item.iloc[:, 0],
-                                        item.iloc[:, 1],
-                                        right=0)
-            #
-            #dfkin.append(new)
-        #
-        return dfkin
-    #
-    #
     def get_kin(self, elev: list, krf: list):
         """
         elev : Elevations
@@ -355,9 +340,22 @@ class KinematicResults:
         #print('-->')
         return kdf
     #
-    # -------------------------------------------------------
+    def kindf(self, items, xx, zdepth):
+        """ """
+        dfkin = np.zeros((xx.size, zdepth.size))
+        for i, step in enumerate(xx):
+            #new = []
+            item = items.get_group(name=(step, ))
+            for j, point in enumerate(zdepth):
+                dfkin[i, j] = np.interp(point,
+                                        item.iloc[:, 0],
+                                        item.iloc[:, 1],
+                                        right=0)
+            #
+            #dfkin.append(new)
+        #
+        return dfkin
     #    
-    #
     def to_xarray(self, rows, cols, length, data, name: str):
         """ """
         #row_meshgrid, col_meshgrid = np.meshgrid(rows, cols, indexing='ij')
@@ -373,6 +371,50 @@ class KinematicResults:
                             name=name)
         #return ds
     #
+    # -------------------------------------------------------
+    #     
+    #
+    def get_kin2(self, beam, nelev: int):
+        """ """
+        xx =  self.surface.x
+        #
+        n1, n2 = beam.nodes
+        sect = beam.section.geometry
+        steps = linstep(d=sect.Dh, L=beam.L, steps=nelev)
+        coord = [n1[:3]]
+        for step in steps[1:]:
+            coord.append(beam.find_coordinate(node_distance=step))
+        #
+        coord = list(map(list, zip(*coord)))
+        #
+        df_mulidx = self._data[['phase', 'x', 'z', 'u', 'ut', 'v', 'vt']].copy()
+        df_mulidx.insert(1, 'y', float(0.0))
+        #df_mulidx.loc[:, 'y'] = float(0.0)
+        df_mulidx.rename(columns={'u': 'ux', 'ut': 'ax',
+                                  'v': 'uz', 'vt': 'az'},
+                         inplace=True)
+        #
+        dataset = {}
+        dfgrp = df_mulidx.groupby('phase') #,'y','z'
+        for key, item in dfgrp:
+            #print(key)
+            data = item.drop(columns=['phase'])
+            data.set_index(['x', 'y', 'z'], inplace=True)
+            #dataset[key] = data.to_xarray()
+            data = data.to_xarray()
+        #
+        #for key, item in dataset.items():
+            #print(f'phase : {key}')
+            # TODO: coord in beam local system
+            dataset[key] = data.interp(x=xr.DataArray(coord[0], dims="coordinates"),
+                                       y=xr.DataArray(coord[2], dims="coordinates"),
+                                       z=xr.DataArray(coord[1], dims="coordinates"),
+                                       method="linear",
+                                       kwargs={"fill_value": 0})
+            #print(dataset[key])
+        #
+        #1 / 0
+        return dataset
     #
     #
 #

steelpy/ufo/mesh/sqlite/beam.py
@@ -319,24 +319,29 @@ class BeamItemSQL(BeamItemBasic):
         self._component = component
         #print('--')
     #
-    @property
-    def number(self) -> int:
+    def __str__(self) -> str:
         """ """
+        beam_name = self.name
         conn = create_connection(self.db_file)
         with conn:
-            data = get_element_data(conn, self.name,
+            data = get_element_data(conn, beam_name,
                                     element_type='beam', 
                                     component=self._component)
-        return data[1]
-    
-    #@number.setter
-    #def number(self, value:int) -> None:
-    #    """"""
-    #    1/0
-    #    conn = create_connection(self.db_file)
-    #    item = "number"
-    #    with conn:
-    #        update_element_item(conn, self.name, item, value)
+        #title =  data[-1]
+        if (title := data[-1]) == None:
+            title = ""
+        #
+        #return "{:8d} {:8d} {:8d} {:>12s} {:>12s} {: 6.4f} {:>6.3f} {:>12s}\n"\
+        #       .format(self.name, *self.connectivity,
+        #               self.material, self.section, self.beta,
+        #               self.length, title)
+        node1, node2 = self.connectivity
+        return "{:>8s} {:>8s} {:>8s} {:>12s} {:>12s} {: 1.2e} {:>1.3e} {:}\n"\
+               .format(str(beam_name), str(node1), str(node2),
+                       str(self.material.name), str(self.section.name),
+                       self.beta, self.L, title)
+    #    
+    # ------------------------------------------------
     #
     @property
     def connectivity(self) -> list:
@@ -365,17 +370,6 @@ class BeamItemSQL(BeamItemBasic):
             update_connectivity(conn, data[1], nnodes)
         #self._connectivity[self.index] = nodes
     #
-    @property
-    def nodes(self) -> list:
-        """
-        """
-        nodes = []
-        conn = create_connection(self.db_file)
-        with conn:
-            for node in self.connectivity:
-                nodes.append(pull_node(conn, node_name=node,
-                                       component=self._component))
-        return nodes
     #
     @property
     def material(self) -> list:
@@ -472,30 +466,30 @@ class BeamItemSQL(BeamItemBasic):
             update_element_item(conn, self.name, item, roll_angle)
     #
     #
-    def __str__(self) -> str:
-        """ """
-        beam_name = self.name
-        conn = create_connection(self.db_file)
-        with conn:
-            data = get_element_data(conn, beam_name,
-                                    element_type='beam', 
-                                    component=self._component)
-        #title =  data[-1]
-        if (title := data[-1]) == None:
-            title = ""
-        #
-        #return "{:8d} {:8d} {:8d} {:>12s} {:>12s} {: 6.4f} {:>6.3f} {:>12s}\n"\
-        #       .format(self.name, *self.connectivity,
-        #               self.material, self.section, self.beta,
-        #               self.length, title)
-        node1, node2 = self.connectivity
-        return "{:>8s} {:>8s} {:>8s} {:>12s} {:>12s} {: 1.2e} {:>1.3e} {:}\n"\
-               .format(str(beam_name), str(node1), str(node2),
-                       str(self.material.name), str(self.section.name),
-                       self.beta, self.L, title)
-    #
     # ------------------------------------------------
     #
+    @property
+    def number(self) -> int:
+        """ """
+        conn = create_connection(self.db_file)
+        with conn:
+            data = get_element_data(conn, self.name,
+                                    element_type='beam', 
+                                    component=self._component)
+        return data[1]
+    #
+    @property
+    def nodes(self) -> list:
+        """
+        """
+        nodes = []
+        conn = create_connection(self.db_file)
+        with conn:
+            for node in self.connectivity:
+                nodes.append(pull_node(conn, node_name=node,
+                                       component=self._component))
+        return nodes
+    #    
     @property
     def DoF(self) -> list[ int ]:
         """
@@ -523,7 +517,6 @@ class BeamItemSQL(BeamItemBasic):
                              component=self._component)
         return dist(node1[:3], node2[:3])
         #return dist(node1[3:6], node2[3:6])
-    #
     #
     @property
     def unit_vector(self) -> list[ float ]:
@@ -561,6 +554,9 @@ class BeamItemSQL(BeamItemBasic):
     #    unitvec = np.array(self.unit_vector)
     #    return Tmatrix(dirCos=unitvec)
     #    #return Tr
+    #
+    # ------------------------------------------------
+    #    
 #
 #
 

steelpy/ufo/process/elements/beam.py
@@ -7,6 +7,7 @@ from array import array
 from dataclasses import dataclass
 from collections.abc import Mapping
 #from typing import NamedTuple
+import math
 
 # package imports
 from steelpy.ufo.mesh.process.bstiffness import beam3D_B3D2, B3D2_Ke, Tmatrix
@@ -85,7 +86,7 @@ class BeamItemBasic:
         self._releases: list[bool] = [False] * 12
         self.type: str = "beam"
     #
-    #
+    # ------------------------------------------------
     # Transformation
     #
     @property
@@ -107,6 +108,7 @@ class BeamItemBasic:
         unitvec = np.array(self.unit_vector)
         return Tmatrix(dirCos=unitvec)
     #
+    # ------------------------------------------------
     # Stiffness
     #
     @property
@@ -165,6 +167,7 @@ class BeamItemBasic:
         #1 / 0
         return k_cond   
     #
+    # ------------------------------------------------
     # Geometry
     #
     def Kg(self, Un: list):
@@ -226,6 +229,7 @@ class BeamItemBasic:
         k_cond = self._k_unc(kg)
         return k_cond
     #
+    # ------------------------------------------------
     # Tangent
     #
     def Kt(self, Un: list):
@@ -278,6 +282,7 @@ class BeamItemBasic:
         k_cond = self._k_unc(kg)
         return k_cond
     #
+    # ------------------------------------------------
     # Mass
     #
     @property
@@ -320,6 +325,7 @@ class BeamItemBasic:
         k_cond = self._k_unc(em)
         return k_cond        
     #
+    # ------------------------------------------------
     # Matrix operations
     #
     def _partition(self, unp_matrix):
@@ -382,6 +388,7 @@ class BeamItemBasic:
             ka = remove_column_row(ka, i, i)
         return ka
     #
+    # ------------------------------------------------
     # Operations
     #
     def intersect_point(self, point:list):
@@ -389,4 +396,41 @@ class BeamItemBasic:
         p1, p2 = self.nodes
         point_line = DistancePointLine3D(p1[:3], p2[:3])
         return point_line.is_on_segment(point[:3])
+    #
+    #
+    def find_coordinate(self, node_distance:float, node_end:int=0) -> tuple:
+        """
+        """
+        node = self.nodes
+        nodeNo3 = [0, 0, 0]
+        #
+        if math.isclose(node_distance, 0, rel_tol=0.01):
+            nodeNo3[0] = node[node_end].x
+            nodeNo3[1] = node[node_end].y
+            nodeNo3[2] = node[node_end].z
+        else:
+            if node_end == 1:
+                v1 = (node[0].x - node[1].x)
+                v2 = (node[0].y - node[1].y)
+                v3 = (node[0].z - node[1].z)
+            else:
+                v1 = (node[1].x - node[0].x)
+                v2 = (node[1].y - node[0].y)
+                v3 = (node[1].z - node[0].z)
+            #
+            norm = (v1**2 + v2**2 + v3**2)**0.50
+            v1 /= norm
+            v2 /= norm
+            v3 /= norm
+            #
+            #L = math.dist(nodei[:3], nodej[:3])
+            #uv = list(map(sub, nodej[:3], nodei[:3]))
+            #l, m, n = [item / L for item in uv]
+            #
+            nodeNo3[0] = (node[node_end].x + v1 * node_distance)
+            nodeNo3[1] = (node[node_end].y + v2 * node_distance)
+            nodeNo3[2] = (node[node_end].z + v3 * node_distance)
+        #
+        return nodeNo3    
+    #
     #